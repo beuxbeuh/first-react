8f040c550561f397b5220cc4cdd3fc761186de34
Benoît Poulain <Benoit.brieg@free.fr>
2022-06-08 06:03:55 -0700

    Création d'un message

---
+++ src/Message/Message.js
@@ -0,0 +1,9 @@
+import './Message.css';
+
+export default function Message() {
+    return (
+        <div className='message'>
+            <h2>Les props dans React</h2>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App/App';
 import Emoji from './Emoji/Emoji';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import Message from './Message/Message';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <Emoji />
+    <Message />    
     <App />
     <Footer />
   </React.StrictMode>
